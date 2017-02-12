20298b61a835104fe5a84ccbafac37802079d9ac
jezzamon <jezzamondev@gmail.com>
2017-02-12 16:56:33 -0500

    Added production build scripts for npm and webpack

---
+++ src/api/userApi.js
@@ -0,0 +1,36 @@
+import 'whatwg-fetch'; //polyfil for browsers that don't support fetch natively
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
+// export one public function
+export function getUsers() {
+  return get('users');
+}
+
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
+function get(url) {
+  return fetch(baseUrl + url)
+    .then(onSuccess, onError);
+}
+
+// can't call func delete since reserved word
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request)
+    .then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+  return response.json();
+}
+
+function onError(error) {
+  console.log(error); // eslint-disable-line no-console
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,  //show what is being bundled
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true,
+      // Properties you define here are available in index.html
+      // using htmlWebpackPlugin.options.varName
+      trackJSToken: '43ad216f57d94259968435894490a5c7'
+    }),
+
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    // Minfify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+};
